925f6fd1cb0b4206ebeb243679ba7002b3e98888
Hector <orantesroman39@gmail.com>
2024-11-28 13:38:28 -0600

    uopdate characters

--- src/app/characters/service/charcaters.service.ts
+++ src/app/characters/service/charcaters.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../enviroments/enviroment'; // Corregido el nombre del archivo de entorno
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CharcatersService {
+export class CharactersService {
+  private apiUrl = `${environment.apiUrl}/api/characters`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCharacters(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+  getCharacterById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
 }
