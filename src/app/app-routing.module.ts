9679b4a2933d83f3d9f6ba3c5da8fad0ccaabc8e
chame <angelchame6@gmail.com>
2024-12-04 23:23:46 -0600

    agregacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardsCharactersComponent } from './characters/cards-characters/cards-characters.component';
 import { PlanetsComponent } from './planets/planets/planets.component';
+
 const routes: Routes = [
   { path: 'characters', component: CardsCharactersComponent },
-  { path: 'planets/:planetName', component: PlanetsComponent }, 
-  { path: '', redirectTo: '/characters', pathMatch: 'full' }, 
-  { path: '**', redirectTo: '/characters' } 
+  { path: 'planets', component: PlanetsComponent },
+  { path: '', redirectTo: '/characters', pathMatch: 'full' },
+  { path: '**', redirectTo: '/characters' }
 ];
 
 @NgModule({
